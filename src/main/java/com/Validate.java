53e7ca7e37dc98c0c1c2469f5cee756ef3daec29
Sylwia Sobieszynska <Sylwia.Sobieszynska@grandparade.co.uk>
2020-12-22 13:54:49 +0100

    setup

---
+++ src/main/java/com/Validate.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ *
+ Write a function which returns if password is valid
+ and write tests to cover cases based on function reqs:
+ More than 3 characters but less than 10.
+ Must contain letters and numbers.
+ There needs to be at least 1 uppercase letter.
+ **/
+
+interface Validate {
+
+     boolean isPasswordValid(String password);
+}
